47dc37ca29703181338187f658a2df30b135f767
Ajmal <dev@ajmalaju.com>
2020-01-13 20:40:39 +0530

    update Export Option In admin panel

--- events/mixins.py
+++ events/mixins.py
@@ -10,7 +10,7 @@ class ExportCsvMixin:
         writer = csv.writer(response)
         writer.writerow(field_names)
         for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])
+            row = writer.writerow([getattr(obj, field) if field != 'branch' and field != 'semester' else getattr(obj, 'get_branch_display')() if field != 'semester' else getattr(obj, 'get_semester_display')() for field in field_names])
         return response
 
     export_as_csv.short_description = "Export Selected"
