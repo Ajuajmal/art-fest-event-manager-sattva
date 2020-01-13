c3aa2dadacb2e8834069747495d1306246179254
Ajmal <dev@ajmalaju.com>
2020-01-13 19:25:40 +0530

    Add pay update Option In admin panel

--- events/admin.py
+++ events/admin.py
@@ -16,8 +16,13 @@ class ParticipantAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('name', 'branch','event','payment')
     list_filter = ('event','regnumber','branch')
     search_fields = ['name', 'regnumber', 'event']
-    actions = ["export_as_csv"]
-
+    actions = ["export_as_csv","update_payment_complete","update_payment_incomplete"]
+    def update_payment_complete(self, request, queryset):
+        queryset.update(payment=True)
+    def update_payment_incomplete(self, request, queryset):
+        queryset.update(payment=False)
+    update_payment_complete.allowed_permissions = ('change',)
+    update_payment_incomplete.allowed_permissions = ('change',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
