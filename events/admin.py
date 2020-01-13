d4fe52f4d175deeb66426a6d6514f893311501f5
Ajmal <dev@ajmalaju.com>
2020-01-13 19:05:57 +0530

    Add Export Option In admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 
+from .mixins import ExportCsvMixin
+
 from .models import Participant,Event,Category
 
-class EventAdmin(admin.ModelAdmin):
+
+class EventAdmin(admin.ModelAdmin,ExportCsvMixin):
     list_display = ('name', 'max_participants')
     list_filter = ('max_participants',)
     prepopulated_fields = {'slug':('name',)}
+    actions = ["export_as_csv"]
 
 
-class ParticipantAdmin(admin.ModelAdmin):
+class ParticipantAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('name', 'branch','event','payment')
     list_filter = ('event','regnumber','branch')
     search_fields = ['name', 'regnumber', 'event']
+    actions = ["export_as_csv"]
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
