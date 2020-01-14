867411a77791dd191667b7d6e7e49c6306e6af31
Ajmal <dev@ajmalaju.com>
2020-01-14 20:09:47 +0530

    update view

--- dashboard/views.py
+++ dashboard/views.py
@@ -30,7 +30,7 @@ class ParticipantListView(LoginRequiredMixin,SingleTableView):
 
 @login_required
 def participant_list(request):
-    table = ParticipantTableCapt(Participant.objects.filter(branch=request.user.profile.branch).filter(deletable=True).order_by('event'))
+    table = ParticipantTableCapt(Participant.objects.filter(branch=request.user.profile.branch).filter(deletable=False).order_by('event'))
     if request.user.is_staff:
         table = ParticipantTableAdmin(Participant.objects.filter(deletable=True).order_by('event'))
     return render(request, "captian_list.html", {
